4fa8b086730f6f3df55bc147c85a479368846fbb
Peshakof <nfs2000@abv.bg>
2020-05-01 22:42:36 +0300

    added media queries to dashboard page

--- client/src/components/dashboard/index.jsx
+++ client/src/components/dashboard/index.jsx
@@ -11,11 +11,10 @@ const Dashboard = () => {
       <ul className="masonry">
         <li className="item">
           <img src="https://i.pinimg.com/236x/a3/2d/51/a32d51ea2f2f5c28db0d07edfacc8973.jpg" alt="" />
-          <p>title goes here</p>
         </li>
-        <li className="item"><img src="https://i.pinimg.com/236x/87/15/51/871551e85dad19deeac033d42a2e6b67.jpg" alt="" /></li>
-        <li className="item"><img src="https://i.pinimg.com/236x/d9/b0/e1/d9b0e1558d9e5b219e41f8916ab4ccaa.jpg" alt="" /></li>
-        <li className="item"><img src="https://i.pinimg.com/236x/5b/27/22/5b27228bc347b780846fc6e3bb340e80.jpg" alt="" /></li>
+        <li className="item"></li>
+        <li className="item"></li>
+        <li className="item"></li>
         <li className="item"><img src="https://i.pinimg.com/236x/23/30/9a/23309a081902fdd8d58abdff812838f7.jpg" alt="" /></li>
         <li className="item"><img src="https://i.pinimg.com/236x/4e/65/79/4e6579a9749d5601529968c683193c1c.jpg" alt="" /></li>
         <li className="item"><img src="https://i.pinimg.com/564x/52/cb/a8/52cba89966d9f8b156e45f0412782001.jpg" alt="" /></li>
